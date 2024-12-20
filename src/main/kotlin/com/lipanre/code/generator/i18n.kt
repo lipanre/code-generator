5cf893e8239ec270978f0ea70a040a31bc57512b
lipan <lipanre@qq.com>
2024-12-21 00:32:15 +0800

    feat: 增加多语言工具类

--- src/main/kotlin/com/lipanre/code/generator/i18n.kt
+++ src/main/kotlin/com/lipanre/code/generator/i18n.kt
@@ -1,7 +1,23 @@
 package com.lipanre.code.generator
 
+import com.intellij.DynamicBundle
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.PropertyKey
+
 /**
  * 多语言
  *
  * @author lipanre
  */
+
+@NonNls
+private const val BUNDLE = "i18n.zh-CN"
+
+/**
+ * 多语言获取工具类
+ */
+object MessageSource: DynamicBundle(BUNDLE) {
+
+    @JvmStatic
+    fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String = getMessage(key, *params)
+}
