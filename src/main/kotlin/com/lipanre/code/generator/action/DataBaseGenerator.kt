4b6904405816f332ff133c1e4b3ddcce1028cd68
lipan <lipanre@qq.com>
2024-12-24 02:20:04 +0800

    feat: 增加输出路径选择面板

--- src/main/kotlin/com/lipanre/code/generator/action/DataBaseGenerator.kt
+++ src/main/kotlin/com/lipanre/code/generator/action/DataBaseGenerator.kt
@@ -3,8 +3,7 @@ package com.lipanre.code.generator.action
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.lipanre.code.generator.DataBaseGeneratorDialog
-import com.lipanre.code.generator.DialogConstant
-import com.lipanre.code.generator.MessageSource
+import java.util.*
 
 /**
  * 数据库代码生成器
@@ -14,11 +13,7 @@ import com.lipanre.code.generator.MessageSource
 class DataBaseGenerator: AnAction() {
 
     override fun actionPerformed(event: AnActionEvent) {
-        DataBaseGeneratorDialog(event.project).run {
-            title = MessageSource.message("title.database-generator")
-            setSize(DialogConstant.WIDTH, DialogConstant.HEIGHT)
-            show()
-        }
+        event.project?.let { DataBaseGeneratorDialog(it) }
     }
 
 }
--- src/main/kotlin/com/lipanre/code/generator/ui.kt
+++ src/main/kotlin/com/lipanre/code/generator/ui.kt
@@ -1,13 +1,17 @@
 package com.lipanre.code.generator
 
-import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.fileChooser.FileChooserDescriptor
+import com.intellij.openapi.fileChooser.PathChooserDialog
+import com.intellij.openapi.fileChooser.ex.FileChooserDialogImpl
+import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.modules
+import com.intellij.openapi.roots.ui.configuration.ChooseModulesDialog
 import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.ui.components.JBPanel
-import com.intellij.ui.components.JBTextField
-import com.intellij.ui.components.Panel
-import com.intellij.ui.dsl.builder.RowLayout
+import com.intellij.openapi.ui.TextFieldWithBrowseButton
+import com.intellij.ui.dsl.builder.Align
 import com.intellij.ui.dsl.builder.panel
+import com.intellij.ui.mac.MacPathChooserDialog
 import javax.swing.JComponent
 
 /**
@@ -15,15 +19,35 @@ import javax.swing.JComponent
  *
  * @author lipanre
  */
-class DataBaseGeneratorDialog(project: Project?) : DialogWrapper(project) {
+class DataBaseGeneratorDialog(
+    private val project: Project,
+    title: String = MessageSource.message("title.database-generator"),
+    width: Int = DialogConstant.WIDTH,
+    height: Int = DialogConstant.HEIGHT
+) : DialogWrapper(project) {
 
     init {
         super.init()
+        setTitle(title)
+        setSize(width, height)
+        show()
     }
 
     override fun createCenterPanel(): JComponent {
-        return panel {
-            row { label("Data-Base-Generator") }
+        val p = panel {
+            row(MessageSource.message("label.please-select-output-path")) {
+                cell(TextFieldWithBrowseButton {
+//                    val fileChooseDescriptor = FileChooserDescriptor(false, true, false, false, false, false)
+//                    val dialog = MacPathChooserDialog(fileChooseDescriptor, null, project)
+                    val modules = ModuleManager.getInstance(project).modules;
+                    val dialog = ChooseModulesDialog(project,
+                        modules.toList(),
+                        "Choose Module", "Choose Single Module");
+                    dialog.setSingleSelectionMode();
+                    dialog.show();
+                }).align(Align.FILL)
+            }
         }
+        return p
     }
 }
