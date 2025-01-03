9d01d35a5064920a4fda054a843234a96195f4b9
lipanre <lipanre@qq.com>
2025-01-03 17:13:56 +0800

    feat: 增加数据库生成模型参数注释

--- src/main/kotlin/com/lipanre/code/generator/model.kt
+++ src/main/kotlin/com/lipanre/code/generator/model.kt
@@ -10,5 +10,8 @@ package com.lipanre.code.generator
 
 /**
  * 数据库配置类
+ * @param outputPath 生成的文件输出路径
+ * @param template   选择使用的模板文件夹名称
+ * @param templateSwitch 模板开关，选择启用哪些模板来生成文件，如果map中不存在默认为需要生成
  */
 data class DataBaseGeneratorConfig(var outputPath: String = "", var template: String? = "", var templateSwitch: MutableMap<String, Boolean> = mutableMapOf())
