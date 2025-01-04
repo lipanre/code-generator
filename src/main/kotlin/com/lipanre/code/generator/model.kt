43abe7bf6300e662bcc0e1bb1c8f4a3c194a854e
lipanre <lipanre@qq.com>
2025-01-04 10:11:51 +0800

    feat: 重构项目结构

--- src/main/kotlin/com/lipanre/code/generator/model.kt
+++ src/main/kotlin/com/lipanre/code/generator/model.kt
@@ -14,4 +14,27 @@ package com.lipanre.code.generator
  * @param template   选择使用的模板文件夹名称
  * @param templateSwitch 模板开关，选择启用哪些模板来生成文件，如果map中不存在默认为需要生成
  */
-data class DataBaseGeneratorConfig(var outputPath: String = "", var template: String? = "", var templateSwitch: MutableMap<String, Boolean> = mutableMapOf())
+data class DataBaseGeneratorConfig(var outputPath: String = "", var template: String? = "", val templateSwitch: MutableMap<String, Boolean> = mutableMapOf()) {
+
+    /**
+     * 模板目录名
+     */
+    private val dirName = "templates"
+
+    /**
+     * 模板列表
+     */
+    val templates by lazy {
+        FileUtil.listScratchesPluginFiles(dirName).toList()
+    }
+
+    /**
+     * 模板名称列表
+     */
+    val templateFileNames: MutableSet<String> = mutableSetOf()
+
+    fun fillTemplateFileNames(fileName: String) {
+        templateFileNames.clear()
+        FileUtil.listScratchesPluginFiles(dirName, fileName).forEach(templateFileNames::add)
+    }
+}
--- src/main/kotlin/com/lipanre/code/generator/ui.kt
+++ src/main/kotlin/com/lipanre/code/generator/ui.kt
@@ -1,9 +1,16 @@
 package com.lipanre.code.generator
 
+import com.intellij.database.util.common.isInstanceOf
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.ui.components.JBCheckBox
+import com.intellij.ui.components.JBPanel
 import com.intellij.ui.dsl.builder.*
+import com.intellij.ui.layout.selected
+import com.intellij.util.ui.CheckBox
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
 import javax.swing.JComponent
 
 /**
@@ -19,49 +26,29 @@ class DataBaseGeneratorDialog(
     height: Int = DialogConstant.HEIGHT,
 ) : DialogWrapper(project) {
 
-    val templateFileNames = mutableSetOf<String>()
-
+    /**
+     * 模板控制开关面板
+     */
+    private lateinit var checkBoxPanel: JBPanel<JBPanel<*>>
 
     /**
-     * 主面板
+     * 下拉框选择器监听器
      */
-    val mainPaine by lazy {
-        panel {
-
-            row(MessageSource.message("title.output-path-file-chooser")) {
-                textFieldWithBrowseButton(MessageSource.message("title.output-path-file-chooser"), fileChooserDescriptor = FileUtil.createFileChooserDescriptor(true)).bindText(config::outputPath).align(Align.FILL)
-            }
-
-            row(MessageSource.message("title.template-select")) {
-                val comboBox = comboBox(templates).bindItem(config::template).align(Align.FILL).component
-                comboBox.selectedItem?.let { fillTemplateFileNames(it as String) }
-                // 点击事件监听
-                comboBox.addActionListener { it -> (it.source as ComboBox<*>).selectedItem?.let { fillTemplateFileNames(it as String) } }
-            }
-
-            group(MessageSource.message("group.template-list")) {
-                row {
-                    templateFileNames.forEach { templateName ->
-                        checkBox(templateName).bindSelected({ config.templateSwitch.getOrDefault(templateName, true) },{ config.templateSwitch[templateName] = it })
-                    }
-                }.rowComment(MessageSource.message("group.template-list-comment"))
-            }
-        }
+    private val comboBoxActionListener: (ActionEvent) -> Unit = {
+        it -> (it.source as ComboBox<*>).selectedItem?.let { config.fillTemplateFileNames(it as String) }
+        updateCheckBoxPanel()
     }
 
     /**
-     * 模板目录名
+     * 多选框checkbox监听器
      */
-    private val dirName = "templates"
+    private val checkBoxActionListener: (ActionEvent) -> Unit = {
+        val checkBox = it.source as JBCheckBox
 
-
-
-    /**
-     * 模板列表
-     */
-    private val templates by lazy {
-        FileUtil.listScratchesPluginFiles(dirName).toList()
+        // 记录开启与关闭的模板
+        config.templateSwitch[checkBox.text] = checkBox.isSelected
     }
+
 
     init {
         super.init()
@@ -70,21 +57,47 @@ class DataBaseGeneratorDialog(
     }
 
     override fun createCenterPanel(): JComponent {
-        return mainPaine
+        return panel {
+            row(MessageSource.message("title.output-path-file-chooser")) {
+                textFieldWithBrowseButton(MessageSource.message("title.output-path-file-chooser"), fileChooserDescriptor = FileUtil.createFileChooserDescriptor(true)).bindText(config::outputPath).align(Align.FILL)
+            }
+            row(MessageSource.message("title.template-select")) {
+                val comboBox = comboBox(config.templates).bindItem(config::template).align(Align.FILL).component
+                comboBox.selectedItem?.let { config.fillTemplateFileNames(it as String) }
+                // 点击事件监听
+                comboBox.addActionListener(comboBoxActionListener)
+            }
+
+            group(MessageSource.message("group.template-list")) {
+                row {
+                    checkBoxPanel = cell(JBPanel<JBPanel<*>>()).component
+                    updateCheckBoxPanel()
+                }.rowComment(MessageSource.message("group.template-list-comment"))
+            }
+        }
     }
 
-    override fun doOKAction() {
-        super.doOKAction()
-        println(config)
+    /**
+     * 更新checkBox面板
+     */
+    private fun updateCheckBoxPanel() {
+        checkBoxPanel.removeAll()
+        config.templateFileNames.forEach {
+            checkBoxPanel.add(createCheckBox(it))
+        }
+        checkBoxPanel.revalidate()
+        checkBoxPanel.repaint()
     }
 
-    override fun show() {
-        println("comboBox select template template name is: $templateFileNames")
-        super.show()
-    }
-
-    private fun fillTemplateFileNames(fileName: String) {
-        FileUtil.listScratchesPluginFiles(dirName, fileName).forEach(templateFileNames::add)
+    /**
+     * 创建checkBox组件
+     */
+    private fun createCheckBox(text: String): JBCheckBox {
+        val checkBox = JBCheckBox(text)
+        // 默认全部是开启状态
+        checkBox.isSelected = true
+        checkBox.addActionListener(checkBoxActionListener)
+        return checkBox
     }
 
 }
